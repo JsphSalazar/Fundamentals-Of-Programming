e08ebd5aaae172ec270ed234bf1ab7dcd2db61e2
JsphSalazar <jsphsalazar@gmail.com>
2019-03-12 23:00:13 -0600

    Refactored

--- WSQ05.py
+++ WSQ05.py
@@ -2,7 +2,7 @@ print ("Hello! This program will calculate the temperature")
 f = input ("What is the temperature in Fahrenheit? ")
 f = float (f)
 c = 5 * (f - 32)/9
-print ("A temperature of",f, "degrees Fahrenheit is",c,"degrees in celsius.")
+print ("A temperature of", f, "degrees Fahrenheit is",c,"degrees in celsius.")
 if(c >= 100):
     print ("Water boils at this temperature.")
 else:
